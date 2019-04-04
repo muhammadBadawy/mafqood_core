5ea3a425913ee5f598c9742f51576f12c087e846
Ahmed Elsharkawy <ahmedelsharkawy>
2019-04-04 20:58:15 +0200

    create strore file for take the path of multiobject and return json of faces prints and can run the file as pyhon storeFaces.py  +imagepath or image location and create comparefaces file which can tach two arg like this pyhon compareFaces.py +object of found faces +object of all missing faces and return json object contain distance between found face with eche face in missing faces

--- sharktask/tensorflow-human-detection.py
+++ sharktask/tensorflow-human-detection.py
@@ -97,7 +97,7 @@ class DetectorAPI:
         #cropped_image.show()
 
 if __name__ == "__main__":
-    print(sys.argv[0])
+    #print(sys.argv[0])
     known_encodings =[]
     known_indexes =[]
     count=0
@@ -105,8 +105,10 @@ if __name__ == "__main__":
     odapi = DetectorAPI(path_to_ckpt=model_path)
     threshold = 0.7
     #1>RGP
-    img=cv2.imread(str(sys.argv[1]), 1)#../TestImages/multiObject.jpg
-    face_detc_image = cv2.imread(str(sys.argv[2]))#../TestImages/faecTest/someOne.jpg
+#    img=cv2.imread(str(sys.argv[1]), 1)#../TestImages/multiObject.jpg
+    img=cv2.imread('../TestImages/multiObject.jpg', 1)
+#    face_detc_image = cv2.imread(str(sys.argv[2]))#../TestImages/faecTest/someOne.jpg
+    face_detc_image = cv2.imread('../TestImages/singleObject.jpg')
     face_detc_image_rgp = cv2.cvtColor(face_detc_image, cv2.COLOR_BGR2RGB)
     test_face_encoding_array = face_recognition.face_encodings(face_detc_image_rgp)[0]
     #print (img)
@@ -135,8 +137,10 @@ if __name__ == "__main__":
                 #np.save('people/face_encoding'+str(i)+'.npy', face_encoding_bytes)
             else:
                 print('there is no faces OR this face not matching')
-    #print (known_encodings)        
+    #print (known_encodings)     
+    print(known_encodings)
     face_distances = face_recognition.face_distance(known_encodings, test_face_encoding_array)
+    print(face_distances)
     
     known_tupples = []
     
